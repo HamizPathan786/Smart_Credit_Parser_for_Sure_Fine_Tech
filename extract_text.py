c22706e65a8cdca96d126ad7a9119ed8fcb9aad5
HamizPathan <hamizp123@gmail.com>
2025-10-15 22:55:56 +0530

    Update extract_text.py

--- extract_text.py
+++ extract_text.py
@@ -1,4 +1,4 @@
-# save as extract_text.py in project root
+
 import pdfplumber
 
 pdf_path = r"C:\Users\hamiz\OneDrive\Desktop\SmartCreditParser\samples\hdfc_sample.pdf"
@@ -8,4 +8,5 @@ with pdfplumber.open(pdf_path) as pdf:
     for page in pdf.pages:
         text += page.extract_text() + "\n"
 
-print(text[:1000])  # print first 1000 chars for inspection
+print(text[:1000])
+
